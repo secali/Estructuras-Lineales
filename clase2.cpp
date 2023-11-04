f9195bbfae127147eecbec0c0b7d8725a24ee643
secali <sergio13896@gmail.com>
2023-11-04 17:34:51 +0100

    -

---
+++ clase2.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //objetos globales std::count y std:cin
+#include <format>   //permite el formato de texto
+#include <string>   //cadenas de caracteres
+#include <vector>   //contenedor secuencial de datos recomendado por defecto en c++
+#include "pel_print.hpp" //libreria para hacer print
+
+using namespace std;
+
+auto main () -> int
+{
+    
+}
